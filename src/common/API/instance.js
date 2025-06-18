1f5acdebcfded6c3c3068b6adf98f4c325681931
nickruden <1nikitos.ru2018@gmail.com>
2025-06-18 14:33:11 +0300

    1

--- src/common/API/instance.js
+++ src/common/API/instance.js
@@ -6,7 +6,7 @@ import { deleteAuthTokens } from "../utils/MyStorage/tokens/deleteAuthTokens";
 import { getNewTokens } from "./services/auth/endpoints";
 
 const apiInstance = axios.create({
-  baseURL: "https://contevents.onrender.com/api/",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
